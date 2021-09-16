a337abca45cea935840cc140738028ed4b9b0bde
Mike Ball <mikedball@gmail.com>
2021-09-16 09:48:19 -0400

    improve error handling

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const fsPromises = fs.promises
 const accessToken = process.env.IG_ACCESS_TOKEN
 
 if (!accessToken) {
-  // TODO
+  console.log(`IG_ACCESS_TOKEN environment variable must be set`)
+  process.exit(1)
 }
 
 async function getRecentMedia() {
   const result = await axios({
     url: `https://graph.instagram.com/me/media?fields=media_url,permalink&access_token=${accessToken}`
   })
+
+  if (result.status >= 400) {
+    console.log(`received ${result.status} from graph.instagram.com`)
+    process.exit(1)
+  }
 
   await fsPromises.writeFile('instagram.json', JSON.stringify(result.data.data));
 }
