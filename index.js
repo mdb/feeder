111af0d5d8c5b748a6a267ac20f97aa16637e11e
Mike Ball <mikedball@gmail.com>
2021-10-10 15:27:34 -0400

    create 'downloadRecentMedia' func

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const path = require('path');
 const fsPromises = fs.promises;
 
 const handleError = (error) => {
@@ -41,14 +42,36 @@ const getRecentMedia = async () => {
       }
     });
 
-    await fsPromises.writeFile('media.json', JSON.stringify(recentMedia));
+    await fsPromises.writeFile(path.join(__dirname, 'media.json'), JSON.stringify(recentMedia));
   } catch (err) {
     module.exports.error(handleError(err));
   }
 };
 
+const downloadRecentMedia = async () => {
+  const media = await fsPromises.readFile('media.json');
+
+  JSON.parse(media).forEach(async (m) => {
+    const fileToSave = await axios({
+      url: m.media_url,
+      responseType: 'stream',
+    });
+    const download = fs.createWriteStream(path.join(__dirname, `${m.id}.jpg`));
+    await new Promise((resolve, reject)=> {
+      fileToSave.data.pipe(download);
+      download.on("close", resolve);
+      download.on("error", console.error);
+    });
+  });
+};
+
+const writeFile = async (media) => {
+  const response = await axios.get(media.media_url);
+  await fsPromises.writeFile(`${media.id}.jpg`, response.data);
+};
+
 if (process.env.NODE_ENV !== 'test') {
-  getRecentMedia();
+  downloadRecentMedia();
 }
 
 module.exports.getRecentMedia = getRecentMedia;
