de58b9fbd51f369b8464c4fdef3cc8ff05b4d853
Mike Ball <mikedball@gmail.com>
2025-09-27 06:22:40 -0400

    fix: IG token refresher no longer 400s
    This also ensures the GHA IG_ACCESS_TOKEN secret is properly set.
    
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- refresh-ig-token.js
+++ refresh-ig-token.js
@@ -2,19 +2,25 @@ const axios = require('axios');
 const core = require('@actions/core');
 
 refreshIgToken = async () => {
-  const result = await axios.get('https://graph.instagram.com/refresh_access_token', {
-    grant_type: 'ig_refresh_token',
-    access_token: process.env.IG_ACCESS_TOKEN,
+  const result = await axios({
+    url: 'https://graph.instagram.com/refresh_access_token',
+    params: {
+      grant_type: 'ig_refresh_token',
+      access_token: process.env.IG_ACCESS_TOKEN
+    }
   });
 
   const token = result.data.access_token;
-  core.setSecret(token)
+  core.setSecret(token);
 
   // test use of the new token & throw error before updating the GHA secret if
   // its use fails.
-  await axios.get('https://graph.instagram.com/v21.0/28395689843380207/media', {
-    access_token: token,
-    fields: 'media_url,caption,permalink'
+  await axios({
+    url: 'https://graph.instagram.com/v21.0/28395689843380207/media',
+    params: {
+      access_token: token,
+      fields: 'media_url,caption,permalink'
+    }
   });
 
   core.setOutput('ig-access-token', token);
